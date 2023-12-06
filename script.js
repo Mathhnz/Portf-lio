121823b43bdd9c404a2faebca41c1ea9c5847f18
Mathhnz <matheusbuenoantunes54321@gmail.com>
2023-12-06 20:08:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function mudarTema() {
+    const body = document.body
+    if(body.classList.contains('escuro')) {
+        body.classList.remove('escuro')
+    } else {
+        body.classList.add('escuro')
+    }
+}
